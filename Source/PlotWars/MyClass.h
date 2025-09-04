8fd702f05660aa663b9c8270e4148e09d209b742
David <155830428+BiermanDavid@users.noreply.github.com>
2025-09-04 14:28:30 +0200

    (Update) Added new test map
    New test map is in place along with a prefab editor level where we can make the buildings. Also discovered on this process that PDA building type needs to NOT take static meshes. If we want to make doors open/close when its a new spawned building, or do something else like a shop counter or working gun store/pawn shop, then you need to instead make new Blueprints for each building type as child elements of BP Building. So that you can retain all of the mesh specific data and set up scripts. for each instance.
    
    Still need to add new plot numbers to each plot

---
+++ Source/PlotWars/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PLOTWARS_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
